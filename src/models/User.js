a042d16e2f1f52b195d67f7ca232bc24d8cdd310
German <gersilva96@hotmail.com>
2020-09-22 13:16:46 -0300

    Add models

---
+++ src/models/User.js
@@ -0,0 +1,34 @@
+import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    roles: [{
+        ref: "Role",
+        type: Schema.Types.ObjectId
+    }]
+}, {
+    timestamps: true,
+    versionKey: false
+});
+
+userSchema.statics.encryptPassword = async password => {
+    return await bcrypt.hash(password, 10);
+};
+
+userSchema.statics.comparePassword = async (password, receivedPassword) => {
+    return await bcrypt.compare(password, receivedPassword);
+};
+
+export default model("User", userSchema);
