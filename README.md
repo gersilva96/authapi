d3d8ace3500eaf9c4de7dab61fe67bb2c625db17
German <gersilva96@hotmail.com>
2020-09-22 13:17:37 -0300

    Add main and config files

---
+++ README.md
@@ -0,0 +1,11 @@
+# API Rest practicando Autenticación con JWT
+
+- Desarrollada en NodeJS con Express
+
+- Se utilizó MongoDB como base de datos NoSQL y Mongoose como ODM
+
+- Se desarrolló con Babel para poder utilizar las últimas características del EcmaScript
+
+- Incluye un simple CRUD de productos, pero se enfoca en la autenticación con JWT
+
+- El despligue se realizó en Heroku y MongoDB Atlas para la base de datos
---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import morgan from "morgan";
+import pkg from '../package.json';
+import { createRoles } from "./libs/initialSetup";
+import productsRouter from "./routes/products.routes";
+import authRouter from "./routes/auth.routes";
+import usersRouter from "./routes/users.routes";
+import "regenerator-runtime";
+
+const app = express();
+createRoles();
+
+app.set("pkg", pkg);
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+    res.json({
+        name: app.get("pkg").name,
+        author: app.get("pkg").author,
+        description: app.get("pkg").description,
+        version: app.get("pkg").version
+    });
+});
+
+app.use("/api/v1/products", productsRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", usersRouter);
+
+export default app;
